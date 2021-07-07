c9bbe8398b938bb90c89030571de6e4a852185cd
Praktikum-krivoruchkoND <krivoruchko.nd@yandex.ru>
2021-07-07 10:19:58 +0300

    some styles and markup done

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,11 +1,20 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
 import pug from 'pug';
 
 import parseDOMFromString from '../../utils/parseDOMFromString';
 
-const template = fs.readFileSync(__dirname + '/login.pug', 'utf-8');
+import logoIcon from '../../assets/icons/logo-icon.svg';
+import './login.scss';
+
+/**
+ * If you want to inline a file into the JavaScript bundle instead 
+ * of reference it by URL, you can use the Node.js fs.readFileSync API to do that. 
+ * The URL must be statically analyzable, meaning it cannot have any variables 
+ * in it (other than __dirname and __filename).
+ */
+const template = readFileSync(__dirname + '/login.pug', 'utf-8');
 
 export default () => {
-  const htmlString = pug.render(template, { logoSrc: 'icons/logo-icon.svg' });
+  const htmlString = pug.render(template, { logoSrc: logoIcon });
   return parseDOMFromString(htmlString);
 };
--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -3,9 +3,12 @@ import pug from 'pug';
 
 import parseDOMFromString from '../../utils/parseDOMFromString';
 
+import logoIcon from '../../assets/icons/logo-icon.svg';
+import './signup.scss';
+
 const template = fs.readFileSync(__dirname + '/signup.pug', 'utf-8');
 
 export default () => {
-  const htmlString = pug.render(template, { logoSrc: 'icons/logo-icon.svg' });
+  const htmlString = pug.render(template, { logoSrc: logoIcon });
   return parseDOMFromString(htmlString);
 };
