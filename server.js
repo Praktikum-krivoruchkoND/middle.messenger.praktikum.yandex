5531c273a5ef6400b720c5194448def17093af31
Praktikum-krivoruchkoND <krivoruchko.nd@yandex.ru>
2021-07-08 16:45:38 +0300

    server fix

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import Bundler from 'parcel-bundler';
-
-const PORT = 3000;
+const express = require('express');
+const path = require('path');
+const port = 3000;
 const app = express();
-const bundler = new Bundler('index.html', { watch: true, cache: false });
 
-app.use(express.static('./dist'));
-app.use(bundler.middleware());
+app.use(express.static(__dirname + '/dist'));
 
-app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'dist',  'index.html'));
+});
+
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
